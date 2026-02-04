556696b7d3fcb7079d3980ad225e88ca16e9d015
suryakamal03 <professionalsurya3@gmail.com>
2026-02-04 19:23:50 +0530

    Replace cube loading animation with simple spinner

--- components/ui/Loading.tsx
+++ components/ui/Loading.tsx
@@ -14,73 +14,26 @@ export default function Loading({ size = 18, className = '' }: LoadingProps) {
           width: size,
           height: size,
         }}
-      >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      />
 
       <style jsx>{`
         .spinner {
-          animation: spinner-y0fdc1 2s infinite ease;
-          transform-style: preserve-3d;
-        }
-
-        .spinner > div {
-          height: 100%;
-          position: absolute;
-          width: 100%;
-          border: 2px solid #1a1a1a;
+          border: 2px solid rgba(0, 0, 0, 0.1);
+          border-top-color: #1a1a1a;
+          border-radius: 50%;
+          animation: spin 0.8s linear infinite;
         }
 
         @media (prefers-color-scheme: dark) {
-          .spinner > div {
-            border-color: #ffffff;
+          .spinner {
+            border-color: rgba(255, 255, 255, 0.1);
+            border-top-color: #ffffff;
           }
         }
 
-        .spinner div:nth-of-type(1) {
-          transform: translateZ(calc(-${size}px / 2)) rotateY(180deg);
-        }
-
-        .spinner div:nth-of-type(2) {
-          transform: rotateY(-270deg) translateX(50%);
-          transform-origin: top right;
-        }
-
-        .spinner div:nth-of-type(3) {
-          transform: rotateY(270deg) translateX(-50%);
-          transform-origin: center left;
-        }
-
-        .spinner div:nth-of-type(4) {
-          transform: rotateX(90deg) translateY(-50%);
-          transform-origin: top center;
-        }
-
-        .spinner div:nth-of-type(5) {
-          transform: rotateX(-90deg) translateY(50%);
-          transform-origin: bottom center;
-        }
-
-        .spinner div:nth-of-type(6) {
-          transform: translateZ(calc(${size}px / 2));
-        }
-
-        @keyframes spinner-y0fdc1 {
-          0% {
-            transform: rotate(45deg) rotateX(-25deg) rotateY(25deg);
-          }
-
-          50% {
-            transform: rotate(45deg) rotateX(-385deg) rotateY(25deg);
-          }
-
-          100% {
-            transform: rotate(45deg) rotateX(-385deg) rotateY(385deg);
+        @keyframes spin {
+          to {
+            transform: rotate(360deg);
           }
         }
       `}</style>
