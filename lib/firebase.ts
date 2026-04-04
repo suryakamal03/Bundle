ba5ac91e867c741ca5d5b475afe777ba8f9b9115
suryakamal03 <professionalsurya3@gmail.com>
2026-04-04 16:05:07 +0530

    ur

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,28 +12,13 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Lazy initialization to prevent build-time errors when env vars are missing
-let _app: FirebaseApp | null = null;
-let _auth: Auth | null = null;
-let _db: Firestore | null = null;
+let app: FirebaseApp;
 
-function getFirebaseApp(): FirebaseApp {
-  if (!_app) {
-    _app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-  }
-  return _app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
 }
 
-export const auth = new Proxy({} as Auth, {
-  get(_, prop) {
-    if (!_auth) _auth = getAuth(getFirebaseApp());
-    return (_auth as any)[prop];
-  },
-});
-
-export const db = new Proxy({} as Firestore, {
-  get(_, prop) {
-    if (!_db) _db = getFirestore(getFirebaseApp());
-    return (_db as any)[prop];
-  },
-});
+export const auth: Auth = getAuth(app);
+export const db: Firestore = getFirestore(app);
